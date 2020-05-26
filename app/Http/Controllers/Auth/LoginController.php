9f906ff046fda4fb19b323a4294a1367fc8e782e
Rezky Maulana <rezkymaulanaa06@gmail.com>
2020-05-27 02:08:13 +0700

    Update auth login 27/05/ 02:08 AM

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request; 
+use Auth;
+use App\User;
 
 class LoginController extends Controller
 {
@@ -36,5 +40,168 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        /** 
+         * Set Type Login
+         * Email or Username
+        */
+        $loginType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        /**
+         * Condition Type Login
+         */
+        if($loginType == 'email')
+        {
+            /** 
+             * Manual email validation
+             */
+            $UsersEmail = User::where('email', $request->username)->get();
+
+            /**
+             * Message Rules
+             * 
+             * @return array
+             */
+            $messages = [
+                
+                "email.email"           => "Format email salah",
+                "email.unique"          => "Email tidak di temukan",
+                "password.required"     => "Kata sandi tidak boleh kosong",
+                "password.min"          => "Kata sandi minimal 4 karakter"
+            ];
+    
+            /** 
+             * Validator Rules
+             * 
+             * @return array
+             */
+            $validator = Validator::make(
+                $request->all(), 
+                [
+                    'email'     => 'email|unique:users,email',
+                    'password'  => 'required|min:4'
+                ], 
+                $messages
+            );
+    
+            /**
+             * Run Authentication Validation
+             * 
+             * @return void
+             */
+            if ($UsersEmail->count() == 0 OR $UsersEmail->count() > 1) 
+            {
+                /**
+                 * If validator false
+                 * 
+                 * @return array
+                 */
+                return back()->withErrors(['email' => 'Email tidak di temukan'])->withInput();
+
+            } else if($validator->fails())
+            {
+                /**
+                 * If validator false
+                 * 
+                 * @return array
+                 */
+                return back()->withErrors($validator)->withInput();
+            }
+            {
+                /**
+                 * Run Auth:attempt
+                 */
+                if (Auth::attempt(['email' => $request->username, 'password' => $request->password, 'account_status' => 'active'])) 
+                {
+                    
+                    /**
+                     * If Login successfuly, redirect to dashboard
+                     */
+                    return redirect()->intended(route('home'));
+                }
+    
+                /**
+                 * If login unsuccessful
+                 */
+                // if unsuccessful -> redirect back
+                return redirect()->back()->withInput($request->only('username'))->withErrors(
+                    [
+                        'password' => 'Kata sandi salah.',
+                    ]
+                );
+            }
+
+        } else if($loginType == 'username')
+        {
+            /**
+             * Message Rules
+             * 
+             * @return array
+             */
+            $messages = [
+                "username.required"     => "Username tidak boleh kosong",
+                "username.min"          => "Username minimal 4 karakter",
+                "username.exists"       => "Username tidak di temukan",
+                "password.required"     => "Kata sandi tidak boleh kosong",
+                "password.min"          => "Kata sandi minimal 4 karakter"
+            ];
+    
+            /** 
+             * Validator Rules
+             * 
+             * @return array
+             */
+            $validator = Validator::make(
+                $request->all(), 
+                [
+                    'username'  => 'required|min:4|exists:users,username',
+                    'password'  => 'required|min:4'
+                ], 
+                $messages
+            );
+    
+            /**
+             * Run Authentication Validation
+             * 
+             * @return void
+             */
+            if ($validator->fails()) 
+            {
+                /**
+                 * If validator false
+                 * 
+                 * @return array
+                 */
+                return back()->withErrors($validator)->withInput();
+
+            } else 
+            {
+                /**
+                 * Run Auth:attempt
+                 */
+                if (Auth::attempt(['username' => $request->username, 'password' => $request->password, 'account_status' => 'active'])) 
+                {
+                    
+                    /**
+                     * If Login successfuly, redirect to dashboard
+                     */
+                    return redirect()->intended(route('home'));
+                }
+    
+                /**
+                 * If login unsuccessful
+                 */
+                // if unsuccessful -> redirect back
+                return redirect()->back()->withInput($request->only('username'))->withErrors(
+                    [
+                        'password' => 'Kata sandi salah.',
+                    ]
+                );
+            }
+        }
+
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,10 +10,10 @@
             <div class="row justify-content-center">
                 <div class="col-md-8 col-lg-6 col-xl-5">
                     <div class="text-center">
-                        <a href="javascript:void(0); class="logo">
+                        <a href="javascript:void(0)"; class="logo">
                             <img src="assets/images/logo-dark.png" alt="" height="22" class="logo-dark mx-auto">
                         </a>
-                        <p class="text-muted mt-2 mb-4">{{ env('APP_NAME') }} VVIP Cheat</p>
+                    <p class="text-muted mt-2 mb-4"></p>
                     </div>
                     <div class="card">
 
@@ -23,20 +23,36 @@
                                 <h4 class="text-uppercase mt-0"> Masuk </h4>
                             </div>
 
+                            @if($errors->any())
+                                {!! implode('', $errors->all('<div class="alert alert-danger">:message</div>')) !!}
+                            @endif
+
                             <form action="{{ route('login') }}" method="POST">
 
                                 {{-- CSRF --}}
                                 {{ csrf_field() }}
 
                                 <div class="form-group mb-3">
-                                    <label for="emailaddress"> Username </label>
-                                    <input class="form-control" type="email" name="username" id="emailaddress" placeholder="Nama pengguna" required>
+                                    <label for="username"> Username </label>
+                                    <input class="form-control {{ $errors->has('username') || $errors->has('email') ? ' is-invalid' : '' }}" type="text" name="username" id="username" value="{{ old('username') ?: old('email') }}" placeholder="Nama pengguna" required>
                                 </div>
+
+                                @if ($errors->has('username') || $errors->has('email'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('username') ?: $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
 
                                 <div class="form-group mb-3">
                                     <label for="password"> Kata sandi </label>
-                                    <input class="form-control" type="password" name="password" id="password" placeholder="Kata sandi" required>
+                                    <input class="form-control @error('password') is-invalid @enderror" type="password" name="password" id="password" placeholder="Kata sandi" required>
                                 </div>
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
 
                                 <div class="form-group mb-0 text-center">
                                     <button class="btn btn-primary btn-block" type="submit"> Masuk </button>

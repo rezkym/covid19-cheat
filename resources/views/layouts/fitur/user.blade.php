b4922739a20e4d2786dacc52615c53366ec5065e
Rezky Maulana <rezkymaulanaa06@gmail.com>
2020-05-30 00:05:51 +0700

    Update user fitur

---
+++ resources/views/layouts/fitur/user.blade.php
@@ -0,0 +1,9 @@
+                                    {{-- Master Fitur --}}
+                                    <li class="has-submenu">
+                                        <a href="#"> <i class="mdi mdi-account-settings"></i> Pengguna <div class="arrow-down"></div></a>
+                                        <ul class="submenu">
+                                            <li>
+                                                <a href="javascript:void(0);"> Ubah HDSN </a>
+                                            </li>
+                                        </ul>
+                                    </li>

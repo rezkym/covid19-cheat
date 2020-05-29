15fc629245a0c0c80de63ae1e44bd77c48a8dc68
Rezky Maulana <rezkymaulanaa06@gmail.com>
2020-05-30 00:19:35 +0700

    update user.blade

--- resources/views/layouts/fitur/user.blade.php
+++ resources/views/layouts/fitur/user.blade.php
@@ -3,7 +3,7 @@
                                         <a href="#"> <i class="mdi mdi-account-settings"></i> Pengguna <div class="arrow-down"></div></a>
                                         <ul class="submenu">
                                             <li>
-                                                <a href="javascript:void(0);"> Ubah HDSN </a>
+                                                <a href="{{ route('change_hdsn') }}"> Ubah HDSN </a>
                                             </li>
                                         </ul>
                                     </li>
